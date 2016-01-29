ef254898f18a3c1c744f12e0da6a7ca366f6de23
Joni Räsänen <jonirsnen@gmail.com>
2016-01-29 23:14:13 +0200

    Script for player movement. Moves +y at constant speed.

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        path = new float[20];
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 currentPosition = transform.position;
+
+        Vector3 target = currentPosition;
+        target.y += speed;
+
+        transform.position = Vector3.Lerp(currentPosition, target, Time.deltaTime);
+	}
+
+    public float[] path;
+}
