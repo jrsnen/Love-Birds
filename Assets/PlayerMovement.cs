8037def97700f4f0e887cbf34744746f8c0be39e
Joni Räsänen <joni.rasanen@iki.fi>
2016-01-30 18:41:41 +0200

    Now birds will DIEEEEEEEE!

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,39 +5,52 @@ public class PlayerMovement : MonoBehaviour {
 
 	public bool facingRight = false;
 	public float speed;
+    public bool dead = false;
+
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 
 	// Update is called once per frame
 	void Update () {
 
-		Vector3 currentPosition = transform.position;
+        if (!dead)
+        {
+            Vector3 currentPosition = transform.position;
 
+            Vector3 target = currentPosition;
+            target.y += speed;
 
-		Vector3 target = currentPosition;
-		target.y += speed;
+            if (Input.GetKey("left") || Input.GetKey("a"))
+            {
+                target.x -= speed;
+                if (facingRight)
+                {
+                    transform.localRotation = Quaternion.Euler(0, 180, 0);
+                    facingRight = false;
+                }
+            }
 
-		if(Input.GetKey("left") || Input.GetKey("a"))
-		{
-			target.x -= speed;
-			if (facingRight) {
-				transform.localRotation = Quaternion.Euler (0, 180, 0);
-				facingRight = false;
-			}
-		}
-
-		if(Input.GetKey("right") || Input.GetKey("d"))
-		{
-			target.x += speed;
-			if (!facingRight) {
-				transform.localRotation = Quaternion.Euler (0, 180, 0);
-				facingRight = true;
-			}
-			transform.localRotation = Quaternion.Euler(0, 0, 0);
-		}
-		transform.position = Vector3.Lerp(currentPosition, target, Time.deltaTime);
+            if (Input.GetKey("right") || Input.GetKey("d"))
+            {
+                target.x += speed;
+                if (!facingRight)
+                {
+                    transform.localRotation = Quaternion.Euler(0, 180, 0);
+                    facingRight = true;
+                }
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+            transform.position = Vector3.Lerp(currentPosition, target, Time.deltaTime);
+        }
 	}
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        dead = true;
+    }
+
+
+    
 }
