7109a393a461b27a4d4cdffc7e1847c4dceefc7c
Joni Räsänen <jonirsnen@gmail.com>
2016-01-30 08:48:47 +0200

    Record player path to an array;

---
+++ Assets/TrackPath.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackPath : MonoBehaviour {
+
+    public uint pathLength = 0;
+
+
+	// Use this for initialization
+	void Start () {
+        Debug.Assert(pathLength != 0);
+        path = new float[pathLength];
+        index = 0;
+        InvokeRepeating("recordPath", 1, 1.0F);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	}
+
+
+    void recordPath()
+    {
+        Debug.Log("Path length: " + index + "/" + pathLength);
+
+        path[index] = transform.position.x;
+
+        ++index;
+    }
+
+    private uint index;
+    public float[] path;
+}
