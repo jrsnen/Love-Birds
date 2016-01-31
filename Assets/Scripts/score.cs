58b110dad30f6857f195b7f21d54e557f8c7c22b
Joni Räsänen <joni.rasanen@iki.fi>
2016-01-31 15:41:38 +0200

    End score script.

---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class score : MonoBehaviour {
+
+    int romeoscore = 0;
+    int juliascore = 0;
+
+    public Text r;
+      public  Text j;
+	// Use this for initialization
+	void Start () {
+         romeoscore = PlayerPrefs.GetInt("romeoScore");
+         juliascore = PlayerPrefs.GetInt("juliaScore");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    j.text =""+ juliascore;
+         r.text =""+ romeoscore;
+	}
+}
