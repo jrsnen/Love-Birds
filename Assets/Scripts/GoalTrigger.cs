a0a9cc90ebff5548466510a3ab44c96c15ebf17e
JuhaniH <juhani.hujala@student.howest.be>
2016-01-31 07:26:50 +0200

    Muuntelin levelii helpommaksi, lisäsin scorea peliruutuun (ei vielä päivity)

---
+++ Assets/Scripts/GoalTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoalTrigger : MonoBehaviour {
+
+    public string tag;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform.tag == tag)
+            Debug.Log("win");
+    }
+}
