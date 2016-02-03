13210e16eab4da76f886e4f24217f62302cccfcb
Joni Räsänen <joni.rasanen@iki.fi>
2016-02-03 22:41:32 +0200

    Finished level generation. Game feels a bit sluggish in the editor. Todo: disable objects not visible?

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -10,6 +10,7 @@ public class LevelGenerator : MonoBehaviour {
     public GameObject background;
 
     public const float lookAhead = 25;
+    public const float maxLookAhead = 75;
 
 
     struct Candidate
@@ -20,10 +21,15 @@ public class LevelGenerator : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        while(yGenerated < lookAhead)
+        bool succeed = false;
+        while (!succeed)
         {
-            yGenerated += GenerateSection();
+            if (!initialized)
+                initializeGeneration();
+            succeed = onePass();
         }
+        createLevel();
+
         juliaMove = julia.GetComponent<PlayerMovement>();
         romeoMove = romeo.GetComponent<PlayerMovement>();
 	}
@@ -46,113 +52,112 @@ public class LevelGenerator : MonoBehaviour {
         }
        
     }
-	
-    uint GenerateSection()
+
+    void initializeGeneration()
     {
         Debug.Log("Generating section");
-
-        bool[] endings = new bool[] { false, false, false, false, false, false, false };
-        bool succeed = false;
-        List<List<bool>> section = new List<List<bool>>();
+        endings = new bool[] { false, false, false, false, false, false, false };
 
         if (yGenerated > 49.9f)
             objectDensity = 0.75f;
 
-
-        while (!succeed)
+        //generate a random level
+        section = new List<List<bool>>();
+        for (uint i = 0; i < 25; ++i)
         {
-            //generate level
-             section = new List<List<bool>>();
-            for (uint i = 0; i < 25; ++i)
-            {
-                bool[] content;
-                if (i == 0)
-                    content = new bool[] { true, 
+            bool[] content;
+            if (i == 0)
+                content = new bool[] { true, 
                     randomBool() && !openings[1], 
                     randomBool() && !openings[2],
                     randomBool() && !openings[3],
                     randomBool() && !openings[4],
                     randomBool() && !openings[5], 
                     true};
-                else
-                    content = new bool[] { true, randomBool(), randomBool(), randomBool(), randomBool(), randomBool(), true };
-                List<bool> row = new List<bool>(new bool[] { content[0], content[1], content[2], content[3], content[4], content[5], content[6] });
-                section.Add(row);
-            }
-
-
-            // check integrity
-            List<Vector2i> candidates = new List<Vector2i>();
-
-
-
-            endings = new bool[] { false, false, false, false, false, false, false };
-            // add start points
-            for (int i = 1; i < 6; ++i)
-            {
-                if (openings[i])
-                {
-                    candidates.Add(new Vector2i(i,0));
-                }
-            }
-
-            uint maxTime = 100;
-
-
-            // go through the candidates until a path is found or all candidates have been exhausted.
-            while (!(endings[1] || endings[2] || endings[3] || endings[4] || endings[5]) 
-                && candidates.Count > 0 && maxTime > 0 )
-            {
-
-                Vector2i current = candidates[candidates.Count - 1];
-
-                candidates.RemoveAt(candidates.Count - 1);
-
-                Debug.Log("Candidate x,y: " + current.x + "," + current.y);
-
-                if(current.y == 24)
-                {
-                    endings[current.x] = true;
-                    break;
-                }
-
-                if(!section[current.y + 1][current.x])
-                {
-                    candidates.Add(new Vector2i(current.x, current.y+1));
-
-                    if(!section[current.y + 1][current.x - 1])
-                    {
-                        candidates.Add(new Vector2i(current.x - 1, current.y + 1));
-                    }
-
-                    if (!section[current.y + 1][current.x + 1])
-                    {
-                        candidates.Add(new Vector2i(current.x + 1, current.y + 1));
-                    }
-                }
-                
-                //Debug.Log("Checking level. Candidates: " + candidates.Count);
-                --maxTime;
-            }
-
-            if (candidates.Count != 0 && maxTime != 0)
-            {
-                Debug.Log("Section accepted!");
-                succeed = true;
-            }
             else
-                Debug.Log("Section discarded!");
-            
+                content = new bool[] { true, randomBool(), randomBool(), randomBool(), randomBool(), randomBool(), true };
+            List<bool> row = new List<bool>( content );
+            section.Add(row);
         }
+
+        // add start points
+        candidates = new List<Vector2i>();
+        for (int i = 1; i < 6; ++i)
+        {
+            if (openings[i])
+            {
+                candidates.Add(new Vector2i(i, 0));
+            }
+        }
+
+        maxTime = 100;
+
+        initialized = true;
+    }
+
+
+
+
+
+
+    // returns whether section is finished
+    bool onePass()
+    {
+
+        Vector2i current = candidates[candidates.Count - 1];
+        candidates.RemoveAt(candidates.Count - 1);
+        Debug.Log("Candidate x,y: " + current.x + "," + current.y);
+
+        if (current.y == 24)
+        {
+            endings[current.x] = true;
+            initialized = false;
+            return true;
+        }
+
+
+        if (!section[current.y + 1][current.x])
+        {
+
+            if (!section[current.y + 1][current.x - 1])
+            {
+                candidates.Add(new Vector2i(current.x - 1, current.y + 1));
+            }
+
+            if (!section[current.y + 1][current.x + 1])
+            {
+                candidates.Add(new Vector2i(current.x + 1, current.y + 1));
+            }
+
+            candidates.Add(new Vector2i(current.x, current.y + 1));
+
+        }
+        --maxTime;
+
+        if (candidates.Count == 0 || maxTime == 0)
+        {
+            initialized = false;
+            Debug.Log("Section rejected!");
+        }
+
+        return false;
+}
+   
+
+
+    // creates level from generation
+    void createLevel()
+    {
 
         openings = endings;
 
+
         // create section
-        for (int i = 0; i < section.Count; ++i )
+        for (int i = 0; i < section.Count; ++i)
         {
-            for(int j = 0; j < section[i].Count; ++j)
+            for (int j = 0; j < section[i].Count; ++j)
             {
-                if(section[i][j])
+                if (section[i][j])
                     Instantiate(bush, new Vector3(j + 0.5f, yGenerated + i, 0), transform.rotation);
             }
         }
@@ -162,8 +167,10 @@ public class LevelGenerator : MonoBehaviour {
         Instantiate(background, new Vector3(3.5f, bgGenerated, 0), transform.rotation);
         Instantiate(background, new Vector3(3.5f, bgGenerated + 12.5f, 0), transform.rotation);
         bgGenerated += 25;
-        return 25;
+        yGenerated += 25;
     }
+
+
 
 
 	// Update is called once per frame
@@ -184,15 +191,44 @@ public class LevelGenerator : MonoBehaviour {
             Debug.Log("ERROR: No active player");
         }
 
-        while(yGenerated < yPosition + lookAhead)
+        //while(yGenerated < yPosition + lookAhead)
+        //{
+        //    yGenerated += GenerateSection();
+        //}
+
+        if (!initialized)
+            initializeGeneration();
+
+        // check if enough tiles are stored
+        if (yGenerated < yPosition + maxLookAhead)
         {
-            yGenerated += GenerateSection();
+            // small update
+            if (yGenerated >= yPosition + lookAhead)
+            {
+                if (onePass())
+                {
+                    createLevel();
+                }
+            }// generate section, we are in a hurry
+            else
+            {
+                bool succeed = false;
+                while (!succeed)
+                {
+                    if (!initialized)
+                        initializeGeneration();
+                    succeed = onePass();
+                }
+                createLevel();
+            }
         }
+
 	}
 
     private float objectDensity = 0.8f;
 
-    private bool[] openings = new bool[]{false,false,false,true,false,false, false};
+    private bool[] openings = new bool[] { false, false, false, true,  false, false, false };
+    private bool[] endings  = new bool[] { false, false, false, false, false, false, false };
 
     private float yGenerated = 0;
     private float bgGenerated = 0;
@@ -201,9 +237,9 @@ public class LevelGenerator : MonoBehaviour {
     private PlayerMovement juliaMove;
     
     
-    
     // temp generation stuff
-
-
-
+    private bool initialized = false;
+    private uint maxTime = 100;
+    private List<List<bool>> section = new List<List<bool>>();
+    private List<Vector2i> candidates = new List<Vector2i>();
 }
